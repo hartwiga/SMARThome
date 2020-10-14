599dac46d6d6ba22ecedad3e89c680bcad2fb2d8
hartwiga <53896748+hartwiga@users.noreply.github.com>
2020-10-14 23:45:20 +0200

    Create config.js

---
+++ www/mobile/js/config.js
@@ -0,0 +1,15 @@
+var config = {
+	app: {
+		userlist: {
+            username: { pw: '336311a016184326ddbdd61edd4eeb52', role: 'admin', name: 'xxxxx' },
+        }
+	},
+	fhemconnector: {
+		url: 'http://10.120.x.xx:8083/fhem/',
+	},
+	surveillancestation: {
+		url: 'http://10.120.x.xx:5000',
+		user: 'admin',
+		password: 'xxxxxxxxxxx',
+	},
+}
